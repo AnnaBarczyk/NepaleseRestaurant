9cc5f02ea60a5e287e9305586bf2e0024deba5c2
Anna Barczyk <anna.ewa.barczyk@gmail.com>
2020-03-06 16:25:41 +0100

    site working, url_for added

--- server.py
+++ server.py
@@ -7,11 +7,20 @@ app = Flask(__name__, template_folder=template_dir, static_folder=STATIC_DIR)
 
 
 @app.route('/')
-def route_list():
+def index():
     return render_template("index.html")
 
+@app.route('/contact.html')
+def contact():
+    return render_template("contact.html")
 
+@app.route('/menu.html')
+def menu():
+    return render_template("menu.html")
 
+@app.route('/about.html')
+def about():
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(
